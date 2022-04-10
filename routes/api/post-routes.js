c7a54df63a524183787e4dcd378b859057b7b3d8
Marcus0224 <marcstlouis04@gmail.com>
2022-04-10 18:50:29 -0400

    created put route for voting on a post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,7 @@
+const sequelize =require('../../config/connection');
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const res = require('express/lib/response');
+const { Post, User, Vote } = require('../../models');
 const { route } = require('./user-routes');
 
 // get all users
@@ -58,6 +60,36 @@ router.get('/', (req, res) => {
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
+      });
+  });
+
+  router.put('/upvote', (req, res) => {
+    Vote.create({
+      user_id: req.body.user_id,
+      post_id: req.body.post_id
+    }).then(() => {
+      // then find the post we just voted on
+      return Post.findOne({
+        where: {
+          id: req.body.post_id
+        },
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          // use row MySQL aggregate function guery to get a count of how many votes the post has and return it under the name 'vote_count'
+          [
+            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+            'vote_count'
+          ]
+        ]
+      })
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err)
       });
   });
 
