59b51327560193ba415b82a7c85e21e2935b9220
Marcus0224 <marcstlouis04@gmail.com>
2022-04-17 19:14:09 -0400

    update main handlebar & home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
         // pass a single post object into the homepage template
         const posts = dbPostData.map(post => post.get({ plain: true }));
 
-        res.render('homepage', {posts});
+        res.render('homepage', {posts, loggedIn: res.session.loggedIn });
       })
       .catch(err => {
         console.log(err);
