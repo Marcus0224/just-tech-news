e62553e7d0b6a24af822a6aa64aeefe2351e8b09
Marcus0224 <marcstlouis04@gmail.com>
2022-04-20 18:51:07 -0400

    add a route

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
-const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const commentRoutes = require('./api/comment-routes');
+const apiRoutes = require('./api');
 
+router.use('./comments', commentRoutes);
+router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
